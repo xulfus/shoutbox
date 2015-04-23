af347f467e5085cce1742a9d05610cbcb429e303
Janne Haarni <janne.haarni@elisa.fi>
2015-04-23 10:38:56 +0300

    more or less final product

--- src/shoutbox/web.clj
+++ src/shoutbox/web.clj
@@ -1,23 +1,50 @@
 (ns shoutbox.web
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [clojure.string :as str]
             [ring.adapter.jetty :as ring]
+            [ring.util.response :as resp]
             [hiccup.page :as page]
-            [shoutbox.dada :as db]))
+            [compojure.route :as route]
+            [compojure.handler :as handler] 
+            [shoutbox.dada :as db]
+            [shoutbox.layout :as layout]
+            [hiccup.core :refer [h]]
+            [hiccup.form :as form]))
 
-(declare home)
+(defn shoutform []
+  [:div {:id "shout-form"}
+   (form/form-to [:post "/"]
+                 (form/label "shout" "What do you want to SHOUT?")
+                 (form/text-field "shout")
+                 (form/submit-button "SHOUT!"))])
+
+(defn display-shouts [shouts]
+  [:div 
+   (map
+    (fn [shout] [:h2 {:class "shout"} (h (:shout shout))])
+    shouts)])
+
+(defn index [shouts]
+  (layout/common "SHOUTER"
+                 (shoutform)
+                 [:div {:class "clear"}]
+                 (display-shouts shouts)))
+
+(defn create-shout [shout]
+  (println "creating shout" shout)
+  (when-not (str/blank? shout)
+    (db/create shout))
+  (resp/redirect "/"))
 
 (defroutes routes
-  (GET "/" [] (home)))
+  (POST "/" [shout] (create-shout shout))
+  (GET "/" [] (index (db/all-shouts)))  
+  (route/resources "/"))
 
-(defn home []
-  (page/html5
-    [:head
-      [:title "Shoutbox"]]
-    [:body
-      [:div {:id "content"} "Hello Shouters"]]))
+(def application (handler/site routes))
 
 (defn -main []
   (when-not (db/db-initialized?)
     (db/initialize-db))
-  (ring/run-jetty #'routes {:port 8080 :join? false}))
+  (ring/run-jetty application {:port 8080 :join? false}))
 
